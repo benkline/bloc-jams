cc62467246da4e0fc3ea7fa0cbfcc4eded23a4df
Ben Kline <ben.kline@gmail.com>
2016-09-06 20:39:59 -0600

    add playerbar 'next' and 'previous' functions

---
+++ scripts/fixtures.js
@@ -0,0 +1,43 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: '4:26' },
+        { title: 'Green', duration: '3:14' },
+        { title: 'Red', duration: '5:01' },
+        { title: 'Pink', duration: '3:21'},
+        { title: 'Magenta', duration: '2:15'}
+    ]
+};
+
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21'},
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15'}
+    ]
+};
+
+var albumTesla = {
+    title: 'The Radio Antenna',
+    artist: 'Nicola Tesla',
+    label: 'RadicalRadio',
+    year: '1929',
+    albumArtUrl: 'assets/images/album_covers/18.png',
+    songs: [
+        { title: 'Kapitalismo? Hell No!', duration: '1:01' },
+        { title: 'Eureka', duration: '5:01' },
+        { title: 'The same thing we do every night Pinky...', duration: '3:21'},
+        { title: 'I will die alone', duration: '3:14' },
+      ]
+};
